260a7ea6f3fc37c44a832b75dca9287dd0b54905
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-04-23 16:26:07 +0400

    added more examples

--- index.js
+++ index.js
@@ -3676,4 +3676,67 @@ function foldTo(distance) {
 console.log(foldTo(348000000))
 
 
+
+function zero(fn) {
+    return fn ? fn(0) : 0
+}
+function one(fn) {
+    return fn ? fn(1) : 1
+}
+function two(fn) {
+    return fn ? fn(2) : 2
+}
+function three(fn) {
+    return fn ? fn(3) : 3
+}
+function four(fn) {
+    return fn ? fn(4) : 4
+}
+function five(fn) {
+    return fn ? fn(5) : 5
+}
+function six(fn) {
+    return fn ? fn(6) : 6
+}
+function seven(fn) {
+    return fn ? fn(7) : 7
+}
+function eight(fn) {
+    return fn ? fn(8) : 8
+}
+function nine(fn) {
+    return fn ? fn(9) : 9
+}
+
+function plus(n) {
+    return function (v) {
+        return v + n
+    }
+}
+function minus(n) {
+    return function (v) {
+        return v - n
+    }
+}
+function times(n) {
+    return function (v) {
+        return v * n
+    }
+}
+function dividedBy(n) {
+    return function (v) {
+        return v / n
+    }
+}
+
+
+
 */
+
+function fuelPrice(litres, pricePerLitre) {
+    if (litres >= 4) return litres * pricePerLitre - litres * 0.1
+}
+
+console.log(fuelPrice(5, 1.23))
+console.log(fuelPrice(8, 2.5))
+console.log(fuelPrice(5, 5.6))
