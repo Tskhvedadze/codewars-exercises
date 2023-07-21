7165def840d5b532174ce40c99c3bce60012a978
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-07-21 21:33:47 +0400

    review && one coding challange

--- index.js
+++ index.js
@@ -4091,4 +4091,98 @@ console.log(
 )
 
 
+
+// ! REVIEW
+let javaScriptIsFun = true
+console.log(javaScriptIsFun)
+
+console.log(typeof true)
+console.log(typeof javaScriptIsFun)
+console.log(typeof 23)
+console.log(typeof 'Jonas')
+
+javaScriptIsFun = 'Yes!'
+console.log(typeof javaScriptIsFun)
+
+let year
+console.log(year)
+console.log(typeof year)
+
+year = 1991
+console.log(year)
+
+console.log(typeof null)
+
+let age = 30
+age = 31
+
+const birthYear = 1991
+
+var job = 'programmer'
+job = 'techer'
+
+
+
+const now = 2037
+
+const ageJonas = now - 1991
+const ageSarah = now - 2018
+
+console.log(ageJonas, ageSarah)
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+
+const firstName = 'Jonas'
+const lastName = 'Schmedtmann'
+
+console.log(firstName + ' ' + lastName)
+
+//? Assigment Operators
+let x = 10 + 5
+x += 10
+x *= 4
+x++
+x--
+
+console.log(x)
+
+//? Comparison operators
+console.log(ageJonas > ageSarah)
+console.log(ageSarah >= 18)
+
+const isFullAge = ageSarah >= 18
+
+console.log(now - 1991 > now - 2018)
+
+
+const now = 2037
+const ageJonas = now - 1991
+const ageSarah = now - 2018
+
+console.log(now - 1991 > now - 2018)
+
+let x, y
+x = y = 25 - 10 - 5
+console.log(x, y)
+
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(averageAge)
+console.log(ageJonas, ageSarah)
+
+
+
+
+//! Chalange
+const marksWeight = 78
+const marksHeight = 1.69
+
+const johnsWeight = 92
+const johnsHeight = 1.95
+
+const MarksBMI = marksWeight / marksHeight ** 2
+const JohnsBMI = johnsWeight / johnsHeight ** 2
+
+console.log(MarksBMI)
+console.log(JohnsBMI)
+
 */
